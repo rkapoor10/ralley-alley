4e4057e454eb09738d074d38325a0396b007dc81
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2022-04-07 21:23:44 +0530

    added: toast

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React from "react";
 import Router from "./router/Router";
 import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -14,6 +16,17 @@ function App() {
       <Link to="/login">Login Form || </Link>
       <Link to="/signup">SignUp Form </Link>
       <Router />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
--- src/components/cart/CartProducts.jsx
+++ src/components/cart/CartProducts.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { MdLocationPin } from "react-icons/md";
-import { BsHeart,BsHeartFill } from "react-icons/bs";
+import { BsHeart, BsHeartFill } from "react-icons/bs";
 import { useCart } from "../../context/cartContext/CartContext";
 import "../../components/products/products.css";
 import DiscountedPrice from "../../utils/DiscountedPrice";
 import { useWishlist } from "../../context/wishlistContext/WishlistContext";
 import { findInWishlist } from "../../utils/findInWishlist";
+import { toast } from "react-toastify";
 
 const CartProducts = () => {
   const { cartState, cartDispatch } = useCart();
-  const {wishlistState,wishlistDispatch} = useWishlist()
-  const {wishlist} = wishlistState
+  const { wishlistState, wishlistDispatch } = useWishlist();
+  const { wishlist } = wishlistState;
   return (
     <div className="child cart-pg-product">
       <div className="space-between alignitems-c wrapit seperator-lightgray padtb-1 mb-2">
@@ -22,7 +23,16 @@ const CartProducts = () => {
 
       <div className="cart-products flex-column-center">
         {cartState.cart.map((product) => {
-          const {id,image,title,brand,categoryName,qty,price,discount} = product
+          const {
+            id,
+            image,
+            title,
+            brand,
+            categoryName,
+            qty,
+            price,
+            discount,
+          } = product;
           return (
             <div key={id} className="card-container-horizontal">
               <div className="flex-txt">
@@ -33,43 +43,79 @@ const CartProducts = () => {
                 />
                 <div>
                   <h3>{title}</h3>
-                  <p className="title-small txt-s mb-1">{brand} <span className="gray">{categoryName}</span></p>
+                  <p className="title-small txt-s mb-1">
+                    {brand} <span className="gray">{categoryName}</span>
+                  </p>
                   <div className="flex-txt mb-1">
-                    <button className="btn-round-small txt-s baseteal" onClick={()=>cartDispatch({type:"ADD_TO_CART",payload:product})}>
+                    <button
+                      className="btn-round-small txt-s baseteal"
+                      onClick={() =>
+                        cartDispatch({ type: "ADD_TO_CART", payload: product })
+                      }
+                    >
                       +
                     </button>
                     <span className="txt-s">{qty}</span>
-                    <button className="btn-round-small txt-s baseteal " onClick={()=>cartDispatch({type:"DECREMENT", payload:product})}>
+                    <button
+                      className="btn-round-small txt-s baseteal "
+                      onClick={() =>
+                        cartDispatch({ type: "DECREMENT", payload: product })
+                      }
+                    >
                       -
                     </button>
                   </div>
                   <p className="mtb-1 price-box txt-xs fw-bold">
-                  {DiscountedPrice(price, discount)} <s className="striked">Rs.{price}</s>
-                    <span className="discount darkorange">({discount}% OFF)</span>
+                    {DiscountedPrice(price, discount)}{" "}
+                    <s className="striked">Rs.{price}</s>
+                    <span className="discount darkorange">
+                      ({discount}% OFF)
+                    </span>
                   </p>
                 </div>
               </div>
               <div>
-                <button className="btn-secondary txt-s" onClick={()=>cartDispatch({type:"REMOVE_FROM_CART",payload:product})}> REMOVE</button>
-                
-                {(findInWishlist(wishlist,product))?<button className="btn-secondary txt-s baseteal" 
-                onClick={() =>
-                    wishlistDispatch({
-                      type: "REMOVE_FROM_WISHLIST",
+                <button
+                  className="btn-secondary txt-s"
+                  onClick={() => {
+                    cartDispatch({
+                      type: "REMOVE_FROM_CART",
                       payload: product,
-                    })
-                  }>
-                  <BsHeartFill />REMOVE FROM WISHLIST</button>:<button className="btn-secondary txt-s baseteal" 
-                onClick={() =>
-                    wishlistDispatch({
-                      type: "ADD_TO_WISHLIST",
-                      payload: product,
-                    })
-                  }>
-                  <BsHeart /> ADD TO YOUR WISHLIST 
-                </button>}
-                
-                
+                    });
+                    toast.success("Removed from Cart");
+                  }}
+                >
+                  REMOVE
+                </button>
+
+                {findInWishlist(wishlist, product) ? (
+                  <button
+                    className="btn-secondary txt-s baseteal"
+                    onClick={() => {
+                      wishlistDispatch({
+                        type: "REMOVE_FROM_WISHLIST",
+                        payload: product,
+                      });
+                      toast.success("Removed from Wishlist");
+                    }}
+                  >
+                    <BsHeartFill />
+                    REMOVE FROM WISHLIST
+                  </button>
+                ) : (
+                  <button
+                    className="btn-secondary txt-s baseteal"
+                    onClick={() => {
+                      wishlistDispatch({
+                        type: "ADD_TO_WISHLIST",
+                        payload: product,
+                      });
+                      toast.success("Added to Wishlist");
+                    }}
+                  >
+                    <BsHeart /> ADD TO YOUR WISHLIST
+                  </button>
+                )}
               </div>
             </div>
           );
@@ -80,5 +126,3 @@ const CartProducts = () => {
 };
 
 export default CartProducts;
-
-
